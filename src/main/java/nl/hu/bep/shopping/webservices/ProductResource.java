74f9a99f910d03ef9b3822a4d15d407ddc764fa9
Jos van Reenen <jos.vanreenen@hu.nl>
2020-05-11 23:16:38 +0200

    ProductResource omgebouwd

--- src/main/java/nl/hu/bep/shopping/webservices/ProductResource.java
+++ src/main/java/nl/hu/bep/shopping/webservices/ProductResource.java
@@ -11,24 +11,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("product")
 public class ProductResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getProducts() {
-        Shop shop = Shop.getShop();
-        JsonArrayBuilder jab = Json.createArrayBuilder();
-
-        for (Product p : shop.getAllProducts()) {
-            JsonObjectBuilder job = Json.createObjectBuilder();
-            job.add("name", p.getName());
-            jab.add(job);
-        }
-
-        JsonArray array = jab.build();
-        return array.toString();
-
+    public Response getProducts() {
+        if (Shop.getShop().getAllProducts().isEmpty()) return Response.noContent().build();
+        return Response.ok(Shop.getShop().getAllProducts()).build();
     }
 }
