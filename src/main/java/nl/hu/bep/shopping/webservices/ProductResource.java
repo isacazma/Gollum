6f6d788a2b9a1465156560bfdb69be9cff90e305
Jos van Reenen <jos.vanreenen@hu.nl>
2020-05-12 00:13:33 +0200

    cleaned

--- src/main/java/nl/hu/bep/shopping/webservices/ProductResource.java
+++ src/main/java/nl/hu/bep/shopping/webservices/ProductResource.java
@@ -1,12 +1,7 @@
 package nl.hu.bep.shopping.webservices;
 
-import nl.hu.bep.shopping.model.Product;
 import nl.hu.bep.shopping.model.Shop;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,4 +17,5 @@ public class ProductResource {
         if (Shop.getShop().getAllProducts().isEmpty()) return Response.noContent().build();
         return Response.ok(Shop.getShop().getAllProducts()).build();
     }
+
 }
